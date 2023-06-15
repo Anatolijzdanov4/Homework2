80a66e1631baf9845a4af79f4b262f4a92e4cf36
anatolij <anatolijzdanov4@gmail.com>
2023-06-15 17:03:32 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,5 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+System.Console.WriteLine("Введите число");
+var result = Convert.ToInt32(Console.ReadLine());
+var number = result/10;
+System.Console.WriteLine($"{number%10}");
